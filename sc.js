2449f86303442807720389454b9211bcc20dfbb7
Shaurya Pratap Singh <66877981+shaurya010@users.noreply.github.com>
2023-07-26 16:16:04 +0530

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,3 +1,21 @@
+// Scrool bar always on top start
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > -1 || document.documentElement.scrollTop > -1) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
+
+
+// Scrool bar always on top end
+
+
+
+
 
 let navbar = document.querySelector('.header .navbar')
 
